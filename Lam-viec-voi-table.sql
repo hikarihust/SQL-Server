3f25651e5441089270fe16ea53ee2e92821186b9
vudinhquang <vudinhquangk53@gmail.com>
2019-01-06 04:01:38 +0900

    lam viec voi table

---
+++ Lam-viec-voi-table.sql
@@ -0,0 +1,36 @@
+use HUST
+
+--create table
+CREATE TABLE Student
+(
+	Id INT,
+	[Name] NVARCHAR(50),
+	[Age] INT,
+	DateOfBirth DATE,
+	Gender BIT
+)
+
+--drop table
+DROP TABLE Student;
+
+--insert data to table
+INSERT INTO Student(Id,	Name, Age, DateOfBirth, Gender) VALUES(1, 'Nguyen Van A', 20, '1990-02-12', 1);
+INSERT INTO Student(Id,	Name, Age, DateOfBirth, Gender) VALUES(2, 'Nguyen Van B', 21, '1991-02-14', 1);
+INSERT INTO Student(Id,	Name, Age, DateOfBirth, Gender) VALUES(3, 'Nguyen Van C', 24, '1990-12-12', 1);
+
+SELECT * From Student1;
+
+--delete table
+DELETE FROM Student WHERE Age > 21;
+
+DELETE FROM Student;
+
+TRUNCATE TABLE Student;
+
+--Rename table
+sp_rename 'Student1', 'Student';
+
+--coppy du lieu tu bang nguon sang bang dich
+SELECT * INTO Student1 FROM Student;
+
+SELECT Id, name INTO Student2 FROM Student1;
